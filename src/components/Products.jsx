49f3f9a709f40ea94df0a464fb715eccec1630a6
Nejla16 <nejla.alicic16@gmail.com>
2025-01-21 12:52:03 +0100

    ReduxToolkit and SingleProduct page

---
+++ src/components/Products.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getSingleProductAction } from "../store/productSlice";
+
+function Products() {
+
+  const {allProducts, isLoading} = useSelector((state) => state.productStore);
+  const dispatch = useDispatch();
+
+  const handleProduct = () => {
+      dispatch(getSingleProductAction({name: 'Tihomir', age:24, adresa:'Doboj'}));
+  }
+
+  return(
+      <div>
+       {/*  {isLoading ? <h2>Loading...</h2> : allProducts.map((product, index) => {
+          return <h2>{product.title}</h2>
+        })} */}
+        <h1>Product</h1>
+        <button onClick={handleProduct}>Get Single Product</button>
+      </div>
+  ) 
+}
+
+export default Products;
